6868831c15cb9550f56b27424ea76ad21d24216b
Elektra Babian <babianelektra@gmail.com>
2020-11-09 16:18:12 -0500

    user search works and if search doesnt exist message is displayed

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -2,28 +2,43 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 
-// let term = '';
-// const apiUrl = `https://goodreads-server-express--dotdash.repl.co/search/${term}`;
-
 
 class App extends React.Component {
   state = {
     data: [],
-    searchTerm: ''
+    searchTerm: '',
+    errorExists: false,
+    errorMessage: ''
   }
 
-  componentDidMount = () => {
-    // event.preventDefault()
-    axios.get(`https://goodreads-server-express--dotdash.repl.co/search/$%7Bterm%7D`).then(
+  accessApiData = (term) => {
+    axios.get(`https://goodreads-server-express--dotdash.repl.co/search/${term}`).then(
       (response) => {
-        // console.log(response);
+        console.log(response);
         this.setState({
           data: response.data.list
         })
       }
     ).catch(error => {
       console.log(error);
+      this.setState({
+        errorExists: true,
+        errorMessage: `${this.state.searchTerm} Does not exist`
+      })
     })
+  }
+
+  userSearchTerm = (event) => {
+    this.setState({
+      searchTerm: event.target.value,
+    })
+    console.log(this.state.searchTerm);
+  }
+
+  searchExists = (event) => {
+    event.preventDefault();
+    this.accessApiData(this.state.searchTerm);
+    console.log('Search Button clicked');
   }
 
   render = () => {
@@ -31,21 +46,28 @@ class App extends React.Component {
       <div className="App">
         <h1>Book List</h1>
         <form>
-          <input type="text" placeholder="Search for a book"/>
-          <input type="submit" value="Search"/>
+          <input
+          onChange={this.userSearchTerm}
+          value={this.state.searchTerm}
+          type="text"
+          placeholder="Search for a book"/>
+          <input onClick={this.searchExists} type="submit" value="Search"/>
         </form>
         <div className="book-container">
-          <ul>
+        {this.state.errorExists ? this.state.errorMessage : ('')}
+        { this.state.data ?
+           <ul>
           {this.state.data.map(
-            (books) => {
-              return <li>
-              <img src={books.imageUrl}/>
-              {books.title}
-              {books.author}
+            (books, index) => {
+              return   <li key={index}>
+                <img alt={books.title} src={books.imageUrl}/>
+                 <h3>{books.title}</h3>
+                <h5>By: {books.authorName}</h5>
               </li>
             }
           )}
           </ul>
+          : ('') }
         </div>
       </div>
     );
